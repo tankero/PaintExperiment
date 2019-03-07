8a675bff7c2bcca15408a6a29a8c61a2f22987a1
cmgonzalez <cmgonzalez@blizzard.com>
2019-03-06 19:01:22 -0800

    More experimentation

--- Assets/TexturePainter.cs
+++ Assets/TexturePainter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,5 +17,34 @@ public class TexturePainter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Caches the target texture and its pixels, to be used in all further texture operations in place of getting the pixels directly to
+    /// dramatically improve performance. Call this before performing multiple consecutive texture operations on the same texture. Each call
+    /// to BeginTextureOperation must follow the operations with a call to CompleteTextureOperation for the changes to be applied.
+    /// 
+    /// Setting overwrite to false will refrain from overwriting an entry for the target texture if one already exists. Use this when
+    /// performing texture operations in Update on the same texture for further optimization.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="overwrite"></param>
+    public static void CacheTexture(Texture2D target, bool overwrite = true)
+    {
+        bool containsKey = TextureCache.Instance.textureCache.ContainsKey(target.GetInstanceID());
+        if ((containsKey && overwrite) || !containsKey)
+        {
+            TextureCache.Instance.textureCache[target.GetInstanceID()] = new Tuple<Texture2D, Color32[]>(target, target.GetPixels32());
+        }
+    }
+
+    public static void UpdateCachedTexture(Texture2D target, Color32[] pixels)
+    {
+        if (TextureCache.Instance.textureCache.ContainsKey(target.GetInstanceID()))
+        {
+            var instance = TextureCache.Instance.textureCache[target.GetInstanceID()];
+            TextureCache.Instance.textureCache[target.GetInstanceID()] = new Tuple<Texture2D, Color32[]>(instance.Item1, pixels);
+
+        }
     }
 }
---
+++ Assets/TextureRevealer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextureRevealer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Paint()
+    {
+        //1. Grab the instance of the target texture (or instantiate it if necessary -- may need some fancy logic for this)
+        //2. Grab the reference ixels from the source texture at the correct location
+        //3. Apply the pixels from the source block to the correct location of the target texture
+
+        //That's a 100% application. If the colors match (a conditional...) increase the alpha instead via lerping.
+
+
+    }
+
+}
