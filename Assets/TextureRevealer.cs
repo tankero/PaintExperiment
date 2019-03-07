1617e8e965847e64aa83f8ba353f112c6bdf09d1
2014PC\Cesar Gonzalez <tankero@gmail.com>
2019-03-07 01:43:50 -0800

    no message

--- Assets/TextureRevealer.cs
+++ Assets/TextureRevealer.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class TextureRevealer : MonoBehaviour
 {
+    public Texture2D background;
+    private Color32[] PixelArray;
+    public Texture2D SourceTexture2D;
+
+    private Color32 caughtPixels;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PixelArray = background.GetPixels32();
     }
 
     // Update is called once per frame
@@ -19,6 +24,8 @@ public class TextureRevealer : MonoBehaviour
 
     private void Paint()
     {
+
+        
         //1. Grab the instance of the target texture (or instantiate it if necessary -- may need some fancy logic for this)
         //2. Grab the reference ixels from the source texture at the correct location
         //3. Apply the pixels from the source block to the correct location of the target texture
@@ -26,6 +33,11 @@ public class TextureRevealer : MonoBehaviour
         //That's a 100% application. If the colors match (a conditional...) increase the alpha instead via lerping.
 
 
+    }
+
+    private Color32[] GetPixelsFromSource()
+    {
+        var offsetPosition = Input.mousePosition;
     }
 
 }
